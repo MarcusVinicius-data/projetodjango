2bc475009168a234ba3c683cccaa84cbfccdc075
MarcusVinicius-data <vinii.moraes@hotmail.com>
2022-12-26 11:12:37 -0300

    Adicionado arquivos estáticos em recipes

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 def home(request):
     return render(
         request,
-        "recipes/home.html",
+        "recipes/pages/home.html",
         context={
             "name": "Marcus Vinicius",
         },
